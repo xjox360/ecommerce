c34b3af51a142e259c0799bd8df345d42d1604b6
jhon gomez <jhongomez360@gmail.com>
2021-03-29 07:35:08 -0500

    Sharing code in Github

---
+++ frontend/src/App.js
@@ -0,0 +1,61 @@
+import "./index.css";
+import data from "./data";
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a href="/" className="brand">
+            cienfit
+          </a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/singin">Sing In</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          {data.products.map((product) => (
+            <div key={product._id} className="card">
+              <a href={`/product/${product._id}`}>
+                <img
+                  className="medium"
+                  src={product.image}
+                  alt={product.name}
+                />
+              </a>
+              <div className="card-body">
+                <a href={`/product/${product._id}`}>
+                  <h2>{product.name}</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+                <div className="price">${product.price}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+      <footer className="row center">All rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Bulsa Top",
+      category: "Shirts",
+      image: "./images/product-1.jpg",
+      price: 120,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "Hermosa blusa con colores vivos que te haran ver fresca y renovada",
+    },
+    {
+      _id: "2",
+      name: "Bulsa Casual",
+      category: "Shirts",
+      image: "./images/product-2.jpg",
+      price: 115,
+      brand: "Adidad",
+      rating: 4,
+      numReviews: 10,
+      description:
+        "Hermosa blusa con colores vivos que te haran ver fresca y renovada",
+    },
+    {
+      _id: "3",
+      name: "Bulsa Mom",
+      category: "Shirts",
+      image: "./images/product-3.jpg",
+      price: 110,
+      brand: "Diadora",
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "Hermosa blusa con colores vivos que te haran ver fresca y renovada",
+    },
+    {
+      _id: "4",
+      name: "Vestido de Novia",
+      category: "Vestidos",
+      image: "./images/product-4.jpg",
+      price: 118,
+      brand: "Nike",
+      rating: 3.5,
+      numReviews: 1,
+      description: "El vestido que hará tus sueños realidad",
+    },
+    {
+      _id: "5",
+      name: "Bulsa Casual",
+      category: "Shirts",
+      image: "./images/product-5.jpg",
+      price: 111,
+      brand: "Arturo Calle",
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "Hermosa blusa con colores vivos que te haran ver fresca y renovada",
+    },
+    {
+      _id: "6",
+      name: "Buso Cuello de Tortuga",
+      category: "Shirts",
+      image: "./images/product-6.jpg",
+      price: 120,
+      brand: "Cate",
+      rating: 5,
+      numReviews: 7,
+      description:
+        "Hermosa blusa con colores vivos que te haran ver fresca y renovada",
+    },
+  ],
+};
+
+export default data;
